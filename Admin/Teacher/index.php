999a3cb101098d7ae5b9265cd5a1cf234d141e00
Sayan2105 <sayannandi.2105@gmail.com>
2024-11-06 14:01:03 +0530

    improved UI

--- Admin/Teacher/index.php
+++ Admin/Teacher/index.php
@@ -24,7 +24,6 @@
     <title>Teacher Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="../styles.css">
 </head>
 <body>
 
